debd97bcee05c230713821cab97929b3753d4fc6
den080 <58463515+den080@users.noreply.github.com>
2026-09-02 22:17:48 +0300

    Update mirror.sh

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
+set -e
 set -x
 echo "FTP_HOST=$FTP_HOST"
 echo "FTP_USER=$FTP_USER"
 echo "FTP_PASS_LEN=${#FTP_PASS}"
-
 cd dist
-ls -la
-
 find . -type f | sed 's|^\./||' | while read -r f; do
   echo "Uploading: $f"
-  curl -v --ftp-pasv --retry 2 --ftp-create-dirs -T "$f" "ftp://$FTP_USER:$FTP_PASS@$FTP_HOST/$f" || {
-    echo "FAILED: $f (exit $?)"
-    exit 1
-  }
+  curl -s --ftp-pasv --retry 2 --ftp-create-dirs --user "$FTP_USER:$FTP_PASS" -T "$f" "ftp://$FTP_HOST/$f" || { echo "FAILED: $f (exit $?)"; exit 1; }
 done
 echo "mirror to reg.ru done"
