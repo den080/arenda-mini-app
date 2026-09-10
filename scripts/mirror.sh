9b5e55b0bf9e8c21092be03ef2903723d8eb12fe
den080 <58463515+den080@users.noreply.github.com>
2026-09-10 21:14:19 +0300

    Update mirror.sh

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd dist
-find . -type f ! -name '.*' ! -size 0 | sed 's|^\./||' | while read -r f; do
-  curl -s --ftp-pasv --ftp-create-dirs --retry 3 --retry-delay 2 --user "$FTP_USER:$FTP_PASS" -T "$f" "ftp://$FTP_HOST/$f" || { echo "FAILED: $f"; exit 1; }
+find . -type f ! -name '.*' ! -name 'telegram-web-app.js' ! -size 0 | sed 's|^\./||' | while read -r f; do
+  curl -s --ftp-pasv --retry 3 --retry-delay 2 --ftp-create-dirs --user "$FTP_USER:$FTP_PASS" -T "$f" "ftp://$FTP_HOST/$f" || { echo "FAILED: $f"; exit 1; }
 done
 echo "mirror to reg.ru done"
